4a6afb07e81f8049d5735dc145feec2ed2682b5d
22-1333 <139518212+22-1333@users.noreply.github.com>
2023-11-08 12:02:38 +0900

    1week revise

--- LWS/1주차/계산.c
+++ LWS/1주차/계산.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
 
 int main(void) {
-    // 입력
+    // 백준 10869 사칙연산
     int A, B;
     scanf("%d %d", &A, &B);
-    
-	// 출력
+
     printf("%d\n", A + B);
     printf("%d\n", A - B);
     printf("%d\n", A * B);
     printf("%d\n", A / B);
     printf("%d\n", A % B);
-    
+
+    // 백준 1008 A / B
+    // 입력
+    double A, B;
+    scanf("%lf %lf", &A, &B);
+
+    // 출력
+    double result = A / B;
+    printf("%.10lf", result);
+
     return 0;
 }
